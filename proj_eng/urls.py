e9440fdbca85c05d1e2478ed24f2707dc5ae861c
Kirill <kir.zajkin75@gmail.com>
2025-04-09 11:44:02 +0300

    Отображение путей к файлам

---
+++ proj_eng/urls.py
@@ -0,0 +1,31 @@
+"""proj_maths URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('terms-list/', views.terms_list, name='terms_list'),
+    path('add-term/', views.add_term, name='add_term'),
+    path('send-term/', views.send_term, name='send_term'),
+    path('stats/', views.show_stats, name='stats'),
+    path('texts-list/', views.texts_list, name='texts_list'),
+    path('test-input/',views.test_input, name='test-input')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
